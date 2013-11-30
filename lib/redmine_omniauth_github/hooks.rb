1af845fdafcfe97f79ed69dfbf45272917b9f6dc
Tim De Pauw <tim.depauw@intec.ugent.be>
2013-11-30 23:31:28 +0100

    Auto-redirect to GitHub

--- lib/redmine_omniauth_github/hooks.rb
+++ lib/redmine_omniauth_github/hooks.rb
@@ -1,5 +1,11 @@
 module RedmineOmniauthGithub
   class Hooks < Redmine::Hook::ViewListener
+    def view_account_login_top(context = {})
+      context[:controller].send(:render_to_string, {
+        :partial => "hooks/view_account_login_top",
+        :locals => context})
+    end
+
     def view_account_login_bottom(context = {})
       context[:controller].send(:render_to_string, {
         :partial => "hooks/view_account_login_bottom",
